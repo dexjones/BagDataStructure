11d2c57b16a3bce6cb5c432a34012a00b5321c01
Dexter <dexter.t.jones@outlook.com>
2024-04-21 20:52:35 -0600

    Completed project

--- Bag/src/Bag.java
+++ Bag/src/Bag.java
@@ -1,2 +1,43 @@
-public class Bag {
+import java.util.*;
+
+public class Bag<T> {
+    private List<T> items;
+
+    // default constructor
+    public Bag() {
+        items = new ArrayList<>();
+    }
+    //add item to bag
+    public void add(T item) {
+        items.add(item);
+    }
+    //remove item from bag
+    public void remove(T item) {
+        items.remove(item);
+    }
+    //return true or false if bag has specific item
+    public boolean contains(T item) {
+        return items.contains(item);
+    }
+    // counts occurences of specified item
+    public int count(T item) {
+        int count = 0;
+        for (int i = 0; i < items.size(); i++ ) {
+            T elem = items.get(i);
+            if (elem.equals(item)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    // returns the size of the bag
+    public int size() {
+        return items.size();
+    }
+
+    // Needed to display the items of the bag instead of memory location
+    @Override
+    public String toString() {
+        return items.toString();
+    }
 }
--- Bag/src/Main.java
+++ Bag/src/Main.java
@@ -1,16 +1,36 @@
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Bag<String> bag = new Bag<>(); // create instance of Bag
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // several items added to bag
+        bag.add("carrots");
+        bag.add("milk");
+        bag.add("cereal");
+        bag.add("grapes");
+        bag.add("carrots");
+        bag.add("banana");
+        bag.add("milk");
+
+        // print the bag contents
+        System.out.println("Bag contents:" + bag);
+
+        // test the contains method
+        System.out.println("Contains 'milk': " + bag.contains("milk"));
+        System.out.println("Contains 'soda': " + bag.contains("soda"));
+
+        // test count method
+        System.out.println("The count of 'carrots' = " + bag.count("carrots"));
+        System.out.println("The count of 'cereal' = " + bag.count("cereal"));
+
+        // test remove method
+        bag.remove("milk");
+        System.out.println("removed 'milk' item from bag:\n" + bag + "\n");
+
+        // test contains method
+        System.out.println("Contains 'milk': " + bag.contains("milk"));
+
+        // test count method
+        System.out.println("The count of 'milk' = " + bag.count("milk"));
     }
 }
