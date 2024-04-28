bfcc61ea74c16ee18f84b02dcff75d416aaa44b4
Dexter <dexter.t.jones@outlook.com>
2024-04-27 20:00:42 -0600

    Completed project

--- Bag/src/Bag.java
+++ Bag/src/Bag.java
@@ -1,43 +1,41 @@
-import java.util.*;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Bag<T> {
-    private List<T> items;
+    private Map<T, Integer> elements;
 
     // default constructor
     public Bag() {
-        items = new ArrayList<>();
+        elements = new HashMap<>();
     }
     //add item to bag
     public void add(T item) {
-        items.add(item);
+        elements.put(item, elements.getOrDefault(item, 0) + 1);
     }
     //remove item from bag
     public void remove(T item) {
-        items.remove(item);
+        if (elements.containsKey(item)) {
+            int count = elements.get(item);
+            if (count > 1) {
+                elements.put(item, count - 1);
+            }
+            else {
+                elements.remove(item);
+            }
+        }
     }
     //return true or false if bag has specific item
     public boolean contains(T item) {
-        return items.contains(item);
+        return elements.containsKey(item);
     }
     // counts occurences of specified item
     public int count(T item) {
-        int count = 0;
-        for (int i = 0; i < items.size(); i++ ) {
-            T elem = items.get(i);
-            if (elem.equals(item)) {
-                count++;
-            }
-        }
-        return count;
+        return elements.getOrDefault(item, 0);
     }
     // returns the size of the bag
-    public int size() {
-        return items.size();
-    }
 
     // Needed to display the items of the bag instead of memory location
     @Override
     public String toString() {
-        return items.toString();
+        return elements.toString();
     }
 }
