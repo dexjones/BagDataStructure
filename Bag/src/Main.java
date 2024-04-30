eb370ace8c71387e8ce41e3d617ab29678a6efe8
Dexter <dexter.t.jones@outlook.com>
2024-04-30 14:11:31 -0600

    Completed project for week 2

--- Bag/src/Main.java
+++ Bag/src/Main.java
@@ -1,36 +1,48 @@
 
 public class Main {
     public static void main(String[] args) {
-        Bag<String> bag = new Bag<>(); // create instance of Bag
+        Bag<String> bag1 = new Bag<>(); // created instances of bag
+        Bag<String> bag2 = new Bag<>();
+        Bag<String> bag3 = new Bag<>();
 
-        // several items added to bag
-        bag.add("carrots");
-        bag.add("milk");
-        bag.add("cereal");
-        bag.add("grapes");
-        bag.add("carrots");
-        bag.add("banana");
-        bag.add("milk");
+        // several items added to bag1
+        bag1.add("carrots");
+        bag1.add("milk");
+        bag1.add("cereal");
+        bag1.add("grapes");
+        bag1.add("carrots");
+        bag1.add("banana");
+        bag1.add("milk");
 
-        // print the bag contents
-        System.out.println("Bag contents:" + bag);
+        // several items added to bag2
+        bag2.add("soda");
+        bag2.add("oreos");
+        bag2.add("pasta");
+        bag2.add("soda");
+        bag2.add("carrots");
+        bag2.add("banana");
+        bag2.add("cranberries");
+        bag2.add("banana");
+        bag2.add("banana");
 
-        // test the contains method
-        System.out.println("Contains 'milk': " + bag.contains("milk"));
-        System.out.println("Contains 'soda': " + bag.contains("soda"));
+        // print size of bag1 and bag2
+        System.out.println("Size of first bag: " + bag1.size());
+        System.out.println("Size of second bag: " + bag2.size());
 
-        // test count method
-        System.out.println("The count of 'carrots' = " + bag.count("carrots"));
-        System.out.println("The count of 'cereal' = " + bag.count("cereal"));
+        // print the bag1 and bag2 contents
+        System.out.println("Bag contents:" + bag1);
+        System.out.println("Bag contents:" + bag2);
 
-        // test remove method
-        bag.remove("milk");
-        System.out.println("removed 'milk' item from bag:\n" + bag + "\n");
+        // merge bag1 and bag2
+        bag1.merge(bag2);
 
-        // test contains method
-        System.out.println("Contains 'milk': " + bag.contains("milk"));
+        //create a new bag containing only distinct elements
+        bag3 = bag1.distinct();
 
-        // test count method
-        System.out.println("The count of 'milk' = " + bag.count("milk"));
+        //print distinct bag
+        System.out.println("Merged bag contents:" + bag3);
+
+        // create a new bag with only distinct elements
+        System.out.println("Distinct bag contents: " + bag1.distinct());
     }
 }
